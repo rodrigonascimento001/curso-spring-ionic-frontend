cdc5d566190acf334a60382f1a6c29b6d1a9dff1
Rodrigo <rodrigo_allexandre@yahoo.com.br>
2020-02-10 14:33:03 -0300

    salvando novo cliente

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -5,7 +5,8 @@ import { CidadeService } from '../../services/domain/cidade.service';
 import { EstadoService } from '../../services/domain/estado.service';
 import { EstadoDTO } from '../../models/estado.dto';
 import { CidadeDTO } from '../../models/cidade.dto';
-
+import { ClienteService } from '../../services/domain/cliente.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @IonicPage()
 @Component({
@@ -19,29 +20,31 @@ export class SignupPage {
   cidades: CidadeDTO[];
 
   constructor(
-    public navCtrl: NavController,
-     public navParams: NavParams,
-     public formBuilder: FormBuilder,
-     public cidadeService: CidadeService,
-     public estadoService: EstadoService) {
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public cidadeService: CidadeService,
+    public estadoService: EstadoService,
+    public clienteService: ClienteService,
+    public alertCtrl: AlertController) {
 
-      this.formGroup = this.formBuilder.group({
-        nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
-        email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
-        tipo : ['1', [Validators.required]],
-        cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
-        senha : ['123', [Validators.required]],
-        logradouro : ['Rua Via', [Validators.required]],
-        numero : ['25', [Validators.required]],
-        complemento : ['Apto 3', []],
-        bairro : ['Copacabana', []],
-        cep : ['10828333', [Validators.required]],
-        telefone1 : ['977261827', [Validators.required]],
-        telefone2 : ['', []],
-        telefone3 : ['', []],
-        estadoId : [null, [Validators.required]],
-        cidadeId : [null, [Validators.required]]      
-      });
+    this.formGroup = this.formBuilder.group({
+      nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
+      tipo : ['1', [Validators.required]],
+      cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha : ['123', [Validators.required]],
+      logradouro : ['Rua Via', [Validators.required]],
+      numero : ['25', [Validators.required]],
+      complemento : ['Apto 3', []],
+      bairro : ['Copacabana', []],
+      cep : ['10828333', [Validators.required]],
+      telefone1 : ['977261827', [Validators.required]],
+      telefone2 : ['', []],
+      telefone3 : ['', []],
+      estadoId : [null, [Validators.required]],
+      cidadeId : [null, [Validators.required]]      
+    });
   }
 
   ionViewDidLoad() {
@@ -65,7 +68,27 @@ export class SignupPage {
   }
 
   signupUser() {
-    console.log('enviou o form');
+    this.clienteService.insert(this.formGroup.value)
+      .subscribe(response => {
+        this.showInsertOk();
+      },
+      error => {});
   }
 
+  showInsertOk() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
